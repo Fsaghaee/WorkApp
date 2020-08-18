898ec0ebcc87f06d7d2677a0341d3129c3a52bb4
Fsaghaee <32325914+Fsaghaee@users.noreply.github.com>
2020-08-18 12:10:39 +0200

    Changed weather Api , added Icon

--- resources/views/Admin/adminPage.blade.php
+++ resources/views/Admin/adminPage.blade.php
@@ -1,9 +1,9 @@
 @extends('Layout.pageLayout')
 @section('MainPart')
     <div style="margin: 20px;">
-    <a href="/logout" >Log out</a>
-    <a href="/admin-drivers"> Drivers</a>
-<a href="/company-works"> All orders</a>
+        <a href="/logout">Log out</a>
+        <a href="/admin-drivers"> Drivers</a>
+        <a href="/company-works"> All orders</a>
 
     </div>
 @stop
@@ -11,50 +11,63 @@
     <div style="margin: 20px;">
 
         <h3>Today : {{date('yy-m-d')}}</h3>
+
+
         <?php
-        $date=strtotime(now()->setTime(16,30,0));
-        $response = file_get_contents('https://api.openweathermap.org/data/2.5/onecall?lat=48.297554&&lon=16.33&dt='.$date.'&units=metric&%20exclude=hourly,daily&appid=ce89196cd42acfa4d84cd561fdb3fde0');
+        $date = date('yy-m-d');
+        $response = file_get_contents('http://api.weatherapi.com/v1/history.json?key=3fa2c903934841ed92885918201808&q=vienna&dt=' . $date);
         $response = json_decode($response, true);
-        echo '<h5>'.date('D',$date). '\'s weather :'. $response['daily'][0]['temp']['max']. ' C    '.$response['daily'][0]['weather'][0]['main'].'</h5>';
-        $Tdate=strtotime('+1 day',strtotime(now()->setTime(16,30,0)));
-        $response = file_get_contents('https://api.openweathermap.org/data/2.5/onecall?lat=48.297554&&lon=16.33&dt='.$Tdate.'&units=metric&%20exclude=hourly,daily&appid=ce89196cd42acfa4d84cd561fdb3fde0');
+        echo date('D') . '  <br>' . $response['forecast']['forecastday'][0]['day']['maxtemp_c'] .
+            ' <br> ' . $response['forecast']['forecastday'][0]['day']['condition']['text'];
+        echo '<img src="' . $response['forecast']['forecastday'][0]['day']['condition']['icon'] . '"/>';
+        echo '<br>';
+        $date = date('yy-m-d', strtotime($date . ' +1 day'));
+        $response = file_get_contents('http://api.weatherapi.com/v1/history.json?key=3fa2c903934841ed92885918201808&q=vienna&dt=' . $date);
         $response = json_decode($response, true);
-        echo '<h5>'.date('D',$date). '\'s weather :';
-        echo $response['daily'][0]['temp']['max']. ' C    ';
-        echo $response['daily'][0]['weather'][0]['main'];
-        echo '</h5>';
+        echo date('D', strtotime(' +1 day')) . '  <br>' . $response['forecast']['forecastday'][0]['day']['maxtemp_c'] .
+            ' <br> ' . $response['forecast']['forecastday'][0]['day']['condition']['text'];
+        echo '<img src="' . $response['forecast']['forecastday'][0]['day']['condition']['icon'] . '"/>';
+
+
+
+
+
+
+
+
 
         ?>
-<br>
+        <br>
 
-<table style="width: 90%;">
-    <tr>
-        <th> Working Day </th>
-        <th> Orders </th>
-        <th> Name </th>
-        <th> Location </th>
-        <th> Weather </th>
-        <th> Temp </th>
-        <th> Hours </th>
-        <th> Break </th>
-        <th> Total </th>
-    </tr>
-@foreach($works as $work)
-    <tr>
-        <td> {{$work->working_day}} </td>
-        <td> {{$work->orders}} </td>
-        <td> {{$work->name}} </td>
-        <td> {{$work->location}} </td>
-        <td> {{$work->wetter_main}} </td>
-        <td> {{$work->wetter_temp}} </td>
-        <td><?php echo abs(strtotime($work->end_working) - strtotime($work->start_working))/(60*60) ?>   </td>
-        <td> {{$work->break}} </td>
-        <td><?php echo abs(strtotime($work->end_working) - strtotime($work->start_working))/(60*60)- $work->break ?>   </td>
-    </tr>
-</table>
+        <table style="width: 90%;">
+            <tr>
+                <th> Working Day</th>
+                <th> Orders</th>
+                <th> Name</th>
+                <th> Location</th>
+                <th> Weather</th>
+                <th> Temp</th>
+                <th> Hours</th>
+                <th> Break</th>
+                <th> Total</th>
+            </tr>
+            @if($works)
+            @foreach($works as $work)
+                <tr>
+                    <td> {{$work->working_day}} </td>
+                    <td> {{$work->orders}} </td>
+                    <td> {{$work->name}} </td>
+                    <td> {{$work->location}} </td>
+                    <td> {{$work->wetter_main}} </td>
+                    <td> {{$work->wetter_temp}} </td>
+                    <td><?php echo abs(strtotime($work->end_working) - strtotime($work->start_working)) / (60 * 60) ?>   </td>
+                    <td> {{$work->break}} </td>
+                    <td><?php echo abs(strtotime($work->end_working) - strtotime($work->start_working)) / (60 * 60) - $work->break ?>   </td>
+                </tr>
+        </table>
     </div>
-@endforeach
-
+    @endforeach
+@endif
 
 @stop
 @section('footer')
--- resources/views/Driver/mainPage.blade.php
+++ resources/views/Driver/mainPage.blade.php
@@ -9,43 +9,49 @@
 @section('centercontent')
     <div class="row">
         <div class="col-6">
-<br>
+            <br>
             <h2>{{auth()->user()->name }}'s Page</h2>
             <br>
-                <?php
+            <?php
+
+            function stringInsert($str, $insertstr, $pos)
+            {
+                $str = substr($str, 0, $pos) . $insertstr . substr($str, $pos);
+                return $str;
+            }
+            $date = date('yy-m-d');
+            // echo $date;
+            $response = file_get_contents('http://api.weatherapi.com/v1/history.json?key=3fa2c903934841ed92885918201808&q=vienna&dt=' . $date);
+            $response = json_decode($response, true);
+            echo date('D') . '  <br>' . $response['forecast']['forecastday'][0]['day']['maxtemp_c'] .
+                ' <br> ' . $response['forecast']['forecastday'][0]['day']['condition']['text'];
+            echo '<img src="' . $response['forecast']['forecastday'][0]['day']['condition']['icon'] . '"/>';
+
+            echo '<br>';
+            $date = date('yy-m-d', strtotime($date . ' +1 day'));
 
 
-                function stringInsert($str, $insertstr, $pos)
-                {
-                    $str = substr($str, 0, $pos) . $insertstr . substr($str, $pos);
-                    return $str;
-                }
 
-                $date = strtotime(now()->setTime(16, 30, 0));
-                $response = file_get_contents('https://api.openweathermap.org/data/2.5/onecall?lat=48.297554&&lon=16.33&dt=' . $date . '&units=metric&%20exclude=hourly,daily&appid=ce89196cd42acfa4d84cd561fdb3fde0');
-                $response = json_decode($response, true);
-                echo '<h7>' . date('D', $date) . '\'s weather :</h7>';
-                echo '<h3>' . $response['daily'][0]['temp']['max'] . ' C';
-                echo '   ';
-                echo $response['daily'][0]['weather'][0]['main'] . '</h3>';
-                $Tdate = strtotime('+1 day', strtotime(now()->setTime(16, 30, 0)));
-                $Tresponse = file_get_contents('https://api.openweathermap.org/data/2.5/onecall?lat=48.297554&&lon=16.33&dt=' . $Tdate . '&units=metric&%20exclude=hourly,daily&appid=ce89196cd42acfa4d84cd561fdb3fde0');
-                $Tresponse = json_decode($Tresponse, true);
-                echo '<h7>' . date('D', $Tdate) . '\'s weather :</h7>';
-                echo '<h3>' . $Tresponse['daily'][0]['temp']['max'] . ' C';
-                echo '   ';
-                echo $Tresponse['daily'][0]['weather'][0]['main'] . '</h3>';
-                echo '</br>';
-                ?>
-                <table style="width: 500px;">
-                    <tr>
-                        <th> Day</th>
+            $Tresponse = file_get_contents('http://api.weatherapi.com/v1/history.json?key=3fa2c903934841ed92885918201808&q=vienna&dt=' . $date);
+            $Tresponse = json_decode($Tresponse, true);
 
-                        <th> Orders</th>
+            echo date('D', strtotime(' +1 day')) . '  <br>' . $Tresponse['forecast']['forecastday'][0]['day']['maxtemp_c'] .
+                ' <br> ' . $Tresponse['forecast']['forecastday'][0]['day']['condition']['text'];
+            echo '<img src="' . $Tresponse['forecast']['forecastday'][0]['day']['condition']['icon'] . '"/>';
 
 
-                    </tr>
 
+            ?>
+            <table style="width: 500px;">
+                <tr>
+                    <th> Day</th>
+
+                    <th> Orders</th>
+
+
+                </tr>
+
+                @if($works)
                     @foreach($works as $work)
                         <tr>
                             <td> {{$work->working_day}} </td>
@@ -54,76 +60,76 @@
                         </tr>
                     @endforeach
 
+                @endif
+            </table>
 
-                </table>
+
+            <table style="width: 500px;">
+                <tr>
+                    <th> File</th>
+
+                    <th> Status</th>
 
 
-                <table style="width: 500px;">
+                </tr>
+@if($slips)
+                @foreach($slips as $slip)
                     <tr>
-                        <th> File</th>
-
-                        <th> Status</th>
-
+                        <td>
+                            <a href=" {{$slip->slip_file_location}}">   <?php echo str_replace(auth()->user()->id . '_', '', str_replace('.pdf', '', str_replace('payslips/', '', $slip->slip_file_location))); ?></a>
+                        </td>
+                        <td> {{$slip->status}} </td>
 
                     </tr>
+                @endforeach
+@endif
 
-                    @foreach($slips as $slip)
-                        <tr>
-                            <td>
-                                <a href=" {{$slip->slip_file_location}}">   <?php echo str_replace(auth()->user()->id . '_', '', str_replace('.pdf', '', str_replace('payslips/', '', $slip->slip_file_location))); ?></a>
-                            </td>
-                            <td> {{$slip->status}} </td>
-
-                        </tr>
-                    @endforeach
+            </table>
 
 
-                </table>
+            <br>
 
-
-                <br>
-
-            </div>
-            <div class="col-6">
-                {!! Form::open(['method'=>'POST','action'=>'DriverPageController@store']) !!}
-
-                {!! form::hidden('working_day',now(),['class'=>'form-control']) !!}
-
-                {!! form::label('orders','Orders :') !!}
-                {!! form::text('orders',null,['class'=>'form-control']) !!}
-                <br>
-
-                {!! form::label('working_account','Account :') !!}
-                {!! form:: select('location',array('FarzadU1'=>'FarzadU1','FarzadU2'=>'FarzadU2','FarzadU3'=>'FarzadU3','FarzadU4'=>'FarzadU4')) !!}
-
-                <br>
-                {!! form::label('start_working','Start Time :') !!}
-                {!! form::time('start_working',now()->setTime(11,0,0),['class'=>'form-control']) !!}
-
-
-                {!! form::label('end_working','End Time :') !!}
-                {!! form::time('end_working',now()->setTime(23,0,0),['class'=>'form-control']) !!}
-                <br>
-                {!! form::label('break','Break :') !!}
-                {!! Form::number('break', null, ['class' => 'form-control','step' => '0.5','min'=>0]) !!}
-
-                <br>
-                {!! form::label('location','Locations :') !!}
-                {!! form:: select('location',array('Klosterneuburg'=>'Klosterneuburg','Wien'=>'Wien')) !!}
-                <br>
-                {!! form::hidden('company_id', auth()->user()->company_id ,['class'=>'form-control']) !!}
-                {!! form::hidden('company_id', auth()->user()->company_id ,['class'=>'form-control']) !!}
-                {!! form::hidden('wetter_temp', $response['daily'][0]['temp']['max'] ,['class'=>'form-control']) !!}
-                {!! form::hidden('wetter_main',$response['daily'][0]['weather'][0]['main'],['class'=>'form-control']) !!}
-
-
-                {!! form::hidden('driver_id', auth()->user()->id ,['class'=>'form-control']) !!}
-
-                {!! form::submit('Add Orders',['class'=>'btn btn-primary']) !!}
-
-
-                {!! Form::close() !!}
-            </div>
         </div>
+        <div class="col-6">
+            {!! Form::open(['method'=>'POST','action'=>'DriverPageController@store']) !!}
+
+            {!! form::hidden('working_day',now(),['class'=>'form-control']) !!}
+
+            {!! form::label('orders','Orders :') !!}
+            {!! form::text('orders',null,['class'=>'form-control']) !!}
+            <br>
+
+            {!! form::label('working_account','Account :') !!}
+            {!! form:: select('location',array('FarzadU1'=>'FarzadU1','FarzadU2'=>'FarzadU2','FarzadU3'=>'FarzadU3','FarzadU4'=>'FarzadU4')) !!}
+
+            <br>
+            {!! form::label('start_working','Start Time :') !!}
+            {!! form::time('start_working',now()->setTime(11,0,0),['class'=>'form-control']) !!}
+
+
+            {!! form::label('end_working','End Time :') !!}
+            {!! form::time('end_working',now()->setTime(23,0,0),['class'=>'form-control']) !!}
+            <br>
+            {!! form::label('break','Break :') !!}
+            {!! Form::number('break', null, ['class' => 'form-control','step' => '0.5','min'=>0]) !!}
+
+            <br>
+            {!! form::label('location','Locations :') !!}
+            {!! form:: select('location',array('Klosterneuburg'=>'Klosterneuburg','Wien'=>'Wien')) !!}
+            <br>
+            {!! form::hidden('company_id', auth()->user()->company_id ,['class'=>'form-control']) !!}
+            {!! form::hidden('company_id', auth()->user()->company_id ,['class'=>'form-control']) !!}
+            {!! form::hidden('wetter_temp', $response['forecast']['forecastday'][0]['day']['maxtemp_c'] ,['class'=>'form-control']) !!}
+            {!! form::hidden('wetter_main',$response['forecast']['forecastday'][0]['day']['condition']['text'],['class'=>'form-control']) !!}
+
+
+            {!! form::hidden('driver_id', auth()->user()->id ,['class'=>'form-control']) !!}
+
+            {!! form::submit('Add Orders',['class'=>'btn btn-primary']) !!}
+
+
+            {!! Form::close() !!}
+        </div>
+    </div>
     </div>
 @stop
