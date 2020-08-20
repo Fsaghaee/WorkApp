ec92bf06cc8728cfebacd72fefef1f9150084eb7
Fsaghaee <32325914+Fsaghaee@users.noreply.github.com>
2020-08-20 17:39:33 +0200

    Sat and Sun Color change

--- resources/views/Admin/CompanyWorks.blade.php
+++ resources/views/Admin/CompanyWorks.blade.php
@@ -2,9 +2,11 @@
 @section('MainPart')
 
     <div style="margin-left: 20px;  border-bottom: 6px solid green;padding: 10px; ">
-        <a   style="font-size:5vw; border: 2px solid green; padding: 10px;margin-right: 10px; "   href="/logout">  Log out  </a>
-        <a style="font-size:5vw; border: 2px solid green; padding: 10px;margin-right: 10px; "   href="/admin">  Main  </a>
-        <a   style="font-size:5vw; border: 2px solid green; padding: 10px;margin-right: 10px; "   href="/admin-drivers">  Drivers  </a>
+        <a style="font-size:5vw; border: 2px solid green; padding: 10px;margin-right: 10px; " href="/logout"> Log
+            out </a>
+        <a style="font-size:5vw; border: 2px solid green; padding: 10px;margin-right: 10px; " href="/admin"> Main </a>
+        <a style="font-size:5vw; border: 2px solid green; padding: 10px;margin-right: 10px; " href="/admin-drivers">
+            Drivers </a>
 
     </div>
 
@@ -12,55 +14,64 @@
 
 @section('centercontent')
     <div style="margin: 20px;">
-    <table style="width: 99%; font-size:2vw;">
-        <tr>
-            <th> Month</th>
-            <th> Day</th>
-            <th> Name</th>
-            <th> Location</th>
-            <th>Account</th>
-            <th> Hours </th>
-
-            <th> Earned </th>
-            <th> Orders</th>
-            <th> Weather </th>
-            <th> Temp </th>
-        </tr>
-        <?php
-        $total = 0;
-        $workingDay = null;
-        ?>
-        @foreach($allworks as $work)
+        <table style="width: 99%; font-size:2vw;">
             <tr>
-                <td>{{date('M',strtotime( $work->working_day ))}}</td>
-                <td> {{date('m-d D',strtotime( $work->working_day ))}} </td>
-                <td> {{$work->name}} </td>
+                <th> Month</th>
+                <th> Day</th>
+                <th> Name</th>
+                <th> Location</th>
+                <th>Account</th>
+                <th> Hours</th>
 
-                <?php
-                $workingDay = $work->working_day;
-                if($work->location == 'Klosterneuburg'){
-                    echo  '<td style="background-color: #6c757d;">'.$work->location.'</td>';
-                }elseif ($work->location == 'Wien'){
-                    echo '<td style="background-color: #1e7e34;">'.$work->location.'</td>';
-                }
-
-                ?>
-
-                <td> {{$work->working_account}} </td>
-                <td><?php echo abs(strtotime($work->end_working) - strtotime($work->start_working))/(60*60)- $work->break ?>   </td>
-                <td> {{$work->orders * 1.3}}  </td>
-                <td> {{$work->orders}} </td>
-                <td> {{$work->wetter_main}} </td>
-                <td> {{$work->wetter_temp}} </td>
+                <th> Earned</th>
+                <th> Orders</th>
+                <th> Weather</th>
+                <th> Temp</th>
             </tr>
-    <?php
-    if (date('d', strtotime($workingDay)) == 15 || date('d', strtotime($workingDay)) == 1) {
-        echo '<tr><td> Total : <td></td></td><td>' . $total . '</td><td>' . $total * 5.4 .' €' . '</td></tr>';
-        $workingDay = null;
-    }
-    ?>
-    @endforeach
+            <?php
+            $total = 0;
+            $workingDay = null;
+            ?>
+            @foreach($allworks as $work)
+                <?php
+                $color = '';
+                if (date('D', strtotime($work->working_day)) == 'Sun' || date('D', strtotime($work->working_day)) == 'Sat') {
+                    $color = '#57b846';
+                } else {
+                    $color = 'white';
+                }
+                ?>
+                <tr style="background-color:<?php echo $color ?>;">
+                    <td>{{date('M',strtotime( $work->working_day ))}}</td>
+                    <td>{{date('m-d D', strtotime($work->working_day))}} </td>
 
-    </table>
+                    <td> {{$work->name}} </td>
+
+                    <?php
+                    $workingDay = $work->working_day;
+                    if ($work->location == 'Klosterneuburg') {
+                        echo '<td style="background-color: #6c757d;">' . $work->location . '</td>';
+                    } elseif ($work->location == 'Wien') {
+                        echo '<td style="background-color: #1e7e34;">' . $work->location . '</td>';
+                    }
+
+                    ?>
+
+                    <td> {{$work->working_account}} </td>
+                    <td><?php echo abs(strtotime($work->end_working) - strtotime($work->start_working)) / (60 * 60) - $work->break ?>   </td>
+                    <td> {{$work->orders * 1.3}}  </td>
+                    <td> {{$work->orders}} </td>
+                    <td> {{$work->wetter_main}} </td>
+                    <td> {{$work->wetter_temp}} </td>
+                </tr>
+                <?php
+                if (date('d', strtotime($workingDay)) == 15 || date('d', strtotime($workingDay)) == 1) {
+                    echo '<tr><td> Total : <td></td></td><td>' . $total . '</td><td>' . $total * 5.4 . ' €' . '</td></tr>';
+                    $workingDay = null;
+                }
+                ?>
+            @endforeach
+
+        </table>
     </div>
 @stop
