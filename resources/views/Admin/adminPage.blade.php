1d7e67e196a59c326824cb5b70ebf45e47dca17c
Fsaghaee <32325914+Fsaghaee@users.noreply.github.com>
2020-09-20 20:35:57 +0200

    Small adjustments

--- resources/views/Admin/adminPage.blade.php
+++ resources/views/Admin/adminPage.blade.php
@@ -2,22 +2,23 @@
 
 @section('centercontent')
 
-
     <?php
+
     $dataPoints01 = array();
     $dataPoints02 = array();
-
     foreach ($klosSum as $k) {
         array_push($dataPoints01, array("y" => $k->total, "label" => date('m.d D', strtotime($k->working_day)), "color" => "black"));
-
     }
     foreach ($WienSum as $k) {
         array_push($dataPoints02, array("y" => $k->total, "label" => date('m.d D', strtotime($k->working_day)), "color" => "black"));
-
     }
-
+    $locat = array();
+    foreach ($locations as $loc) {
+        array_push($locat, $loc->location);
+    }
+    //array('Klosterneuburg', 'Wien', 'Tulln');
+    $Days = array('Mon', 'Din', 'Mit', 'Don', 'Fri', 'Sam', 'Sun');
     ?>
-
     <div style="margin: 5px;">
         <div style="margin: 0;width: 100%; background-color: gray; width: 100%; height: 50px;color: white;">
 
@@ -63,21 +64,9 @@
                 </div>
             </div>
         </div>
+
         <div id="Table" style="display: none;">
-
-            <?php
-            $locat = array();
-            foreach ($locations as $loc) {
-                array_push($locat, $loc->location);
-            }
-
-            //array('Klosterneuburg', 'Wien', 'Tulln');
-            $Days = array('Mon', 'Din', 'Mit', 'Don', 'Fri', 'Sam', 'Sun');
-            ?>
-
-
-            <table width="100%;">
-
+            <table style="width: 100%;">
                 <tr>
                     <th style="text-align: center;"> Day</th>
                     <?php
@@ -86,7 +75,6 @@
                         echo " <th style=\"text-align: center;\">$temp - VorM.</th><th style=\"text-align: center;\"> $temp - NachM.</th>";
                     }
                     ?>
-
                 </tr>
 
                 <?php
@@ -97,31 +85,22 @@
                     for ($j = 0; $j < count($locat); $j++) {#
                         echo "<td>";
                         $tempinput = $locat[$j] . 'v' . $Days[$i];
-
                         echo " <div id='$tempinput'>";
                         echo "<input type=\"button\" value=\" + \" onClick=\"addNew('$tempinput');\" style='width: 95%;margin:0 5px; border: 1px solid lightgray; background-color: lightgray;border-radius: 15px;'>";
                         echo "</div>";
                         echo "</td>";
                         echo "<td>";
-
-
                         $tempinput = $locat[$j] . 'n' . $Days[$i];
                         echo " <div id='$tempinput'>";
                         echo "<input type=\"button\" value=\" + \" onClick=\"addNew('$tempinput');\"style='width: 95%;margin: 5px; border: 1px solid lightgray;background-color: lightgray;border-radius: 15px;'>";
                         echo "</div>";
                         echo "</td>";
-
                     }
 
                     echo "</tr>";
                 }
-
-
                 ?>
-
             </table>
-
-
             <table
                 style="width: 100%; font-size:2vw; border-top: 6px solid darkgray; margin:0;padding: 0 10px;color: white;background-color: gray;min-height: 50px;">
                 @if($works)
@@ -133,19 +112,13 @@
                             <td> {{$work->wetter_main}} </td>
                             <td> {{$work->wetter_temp}} </td>
                         </tr>
-
-
                     @endforeach
                 @endif
             </table>
         </div>
         <div class="row">
-
             <div class="col">
-
                 <?php
-
-
                 echo '<h6 style="font-size:3vw;">';
                 $date = date('yy-m-d');
                 $response = file_get_contents('http://api.weatherapi.com/v1/history.json?key=3fa2c903934841ed92885918201808&q=vienna&dt=' . $date);
@@ -164,32 +137,30 @@
                 echo '<img src="' . $response['forecast']['forecastday'][0]['day']['condition']['icon'] . '"/>';
                 echo '</h6>';
                 ?>
-
             </div>
         </div>
-
         <div class="row">
-
             <div class="col" id="Kloster" style="height: 270px; width: 100%; padding: 5px;"></div>
             <div class="col" id="wien" style="height: 270px; width: 100%; padding: 5px;"></div>
             <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
         </div>
-
-
     </div>
-
     <script>
+
+
         function addNew(pos) {
 
             var mainContainer = document.getElementById(pos);
             var newDiv = document.createElement('div');
+            newDiv.style.marginBottom="5px";
             var newDropdown = document.createElement('select');
-            newDropdown.style.height = "40px";
+
             newDropdown.style.float = "left";
-           // newDropdown.style.minWidth = "75% !important";
-            newDropdown.setAttribute('style','min-width: 75% !important;margin-left:5px;');
-            newDropdown.style.fontSize = "2vw";
-            newDropdown.style.padding = "5px";
+
+            // newDropdown.style.minWidth = "75% !important";
+            newDropdown.setAttribute('style', 'min-width: 75% !important;margin-left:5px;height:35px;');
+            newDropdown.style.fontSize = "1.2vw";
+            newDropdown.style.padding = "2px";
             newDropdown.style.top = "0";
             newDropdown.style.borderRadius = "15px";
             newDropdown.style.backgroundColor = "gray";
@@ -197,15 +168,22 @@
                 <?php
                 $z = 1;
                 foreach ($allDrivers as $driver) {
+                    $colortemp = "white";
                     echo "
+
            newDropdownOption$z = document.createElement('option');
-           newDropdownOption$z.value = '$driver->name';
+           newDropdownOption$z.value = '$driver->id';
            newDropdownOption$z.text = '$driver->name';
+           newDropdownOption$z.style.color='$colortemp';
+           newDropdownOption$z.style.textAlign='center';
            newDropdown.add(newDropdownOption$z);
         ";
                     $z++;
                 }
                 ?>
+
+
+
             var newAddButton = document.createElement('input');
             newAddButton.type = "button";
             newAddButton.value = " + ";
--- resources/views/Layout/pageLayout.blade.php
+++ resources/views/Layout/pageLayout.blade.php
@@ -22,10 +22,12 @@
     <!--===============================================================================================-->
     <link rel="stylesheet" type="text/css" href="css/util.css">
     <link rel="stylesheet" type="text/css" href="css/main.css">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
 </head>
+
 <style>
     body html{
         font-family: Raleway-Regular, sans-serif !important;
